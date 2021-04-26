063f66d6e11ae39c5b4482c164220b67568acdad
MatosLeo <leonardoresendematos@gmail.com>
2021-04-25 21:56:31 -0300

    Check-in inicial do módulo Teste_do_Léo

---
+++ Teste_do_Léo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Teste_do_Léo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minhavariavel1 = 5;
+            int minhavariavel2 = 6;
+            int minhavariavel3 = minhavariavel1 * minhavariavel2;
+            int minhavariavel4 = minhavariavel3 / minhavariavel1;
+         
+
+            Console.WriteLine("sua variável 1 é {0} e sua variável 2 é {1}. O resultado de sua multiplicação é {2}, já o resultado de sua divisão é {3}",minhavariavel1,minhavariavel2,minhavariavel3,minhavariavel4);
+            
+        }
+    }
+}
---
+++ Teste_do_Léo2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste_do_Léo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int my1 = 5 % 2;
+            Console.WriteLine(my1);
+        }
+    }
+}
